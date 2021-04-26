fdb7aa13be7c732dbfcf6d2c25fddfe167ead2c8
binag123 <67109413+binag123@users.noreply.github.com>
2021-04-26 16:33:58 +1000

    add the code

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -8,7 +8,7 @@ import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import AlbumScreen from '../screens/AlbumScreen'
 import AlbumDetailScreen from '../screens/AlbumDetailScreen'
-
+import StorageScreen from '../screens/StorageScreen'
 import FavoriteScreen from '../screens/FavoriteScreen';
 
 const config = Platform.select({
@@ -49,6 +49,15 @@ const LinksStack = createStackNavigator(
   config
 );
 
+LinksStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+    Storage:StorageScreen
+  },
+  config
+);
 LinksStack.navigationOptions = {
   tabBarLabel: 'Links',
   tabBarIcon: ({ focused }) => (
